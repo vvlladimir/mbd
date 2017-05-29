c8d15a9d5b3627515c58b86a2c2b35152b1cfaf0
Vladimir Todorovic <vvlladimir@gmail.com>
2017-05-29 02:25:05 +0200

    slideToggle

--- app/js/script.js
+++ app/js/script.js
@@ -17,31 +17,34 @@ var settings = {
 $.ajax(settings).done(function (response) {
 
 
-  $('#movies').on('click',function(a) {
+  $('#movies').click('click',function(a) {
+    a.stopPropagation();
 
-      a.preventDefault();
+  $('#resultsMovies').slideToggle("fast", function(){
+      $('#movies').toggleClass('active');
+      for(var i = 0, l = response.results.length/2; i < l; i++) {
+        var resultShow = '<ul>';
+        resultShow += '<li><a id="resultLink" href="https://api.themoviedb.org/3/movie/' + response.results[i].id + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" class="result-row"><img src="https://image.tmdb.org/t/p/w500' + response.results[i].poster_path + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" height="120px" /> ' + response.results[i].title + '</a></li>';
+        resultShow += '</ul>';
+        $('#resultsMovies').append(resultShow);
+      }
+  });
 
-        $('#movies').toggleClass('active');
-
-        for(var i = 0, l = response.results.length/2; i < l; i++) {
-          var resultShow = '<ul>';
-          resultShow += '<li><a id="resultLink" href="https://api.themoviedb.org/3/movie/' + response.results[i].id + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" class="result-row"><img src="https://image.tmdb.org/t/p/w500' + response.results[i].poster_path + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" height="120px" /> ' + response.results[i].title + '</a></li>';
-          resultShow += '</ul>';
-          $('#results').append(resultShow);
-        }
-
-});
-
-$('#resultLink').on('click', function(){
-  var movieContainer = '<ul>';
-  movieContainer += '<li>' + response.results[i].title + '</li>';
-  movieContainer += '</ul>';
-  $('#movieDetalsContainer').append(movieContainer);
 
 });
 
-});//ajax settings
+// $('#resultLink').on('click', function(){
+//   var movieContainer = '<ul>';
+//   movieContainer += '<li>' + response.results[i].title + '</li>';
+//   movieContainer += '</ul>';
+//   $('#movieDetalsContainer').append(movieContainer);
+//
+// });
 
+});
+
+
+//tv show popular btn and content
 var tvPopular = {
     "async": true,
     "crossDomain": true,
@@ -53,55 +56,24 @@ var tvPopular = {
 
 $.ajax(tvPopular).done(function (response) {
 
-    $('#tvShows').on('click',function(e) {
-        e.preventDefault();
+    $('#tvShows').click('click',function(e) {
+        e.stopPropagation();
+
+        $('#resultsTvShows').slideToggle("fast", function(){
         $('#tvShows').toggleClass('active');
         for(var i = 0, l = response.results.length/2; i < l; i++) {
           var resultTvShow = '<ul>';
           resultTvShow += '<li><a id="resultLink" href="https://api.themoviedb.org/3/tv/' + response.results[i].id + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" class="result-row"><img src="https://image.tmdb.org/t/p/w500' + response.results[i].poster_path + '?api_key=9e93a8072a4001746ef7cd2093ad1e4a&language=en-US" height="120px" /> '+ response.results[i].name +'</a></li>';
           resultTvShow += '</ul>';
-
-          $('#results').append(resultTvShow);
-
-
-        }
-    });
-});
-
-//Searching movies from the input field
-
-
-$('#searchMovie').keydown(function(){
-
-    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=9e93a8072a4001746ef7cd2093ad1e4a&query=data",function(data){
-
-      var search = $('#searchMovie').val();
-
-      var regex = new RegExp(search, 'i');
-
-      $.each(data, function(key, val){
-
-//console.log(this);
-        if((results.title.search(regex)) != -1 ){
-
-          for(var i = 0, l = results.length/1; i < l; i++) {
-            var output;
-            output += '<ul>';
-             output += '<li>';
-             output += '<h2>' + val.results[i].title + '</h2>';
-
-             output += '</li>';
-             output += '</ul>';
-             $('#results').append(output);
-
-             }
+          $('#resultsTvShows').append(resultTvShow);
         }
       });
     });
-  })
+});
 
 
 
+//Searching movies from the input field
 
 // var searchMovie = {
 //     "async": true,
@@ -111,31 +83,67 @@ $('#searchMovie').keydown(function(){
 //     "headers": {},
 //     "data": "{}"
 // }
-
-// $.ajax(searchMovie).done(function (response) {
-// console.log(this);
-//                $('#searchMovie').keyup(function() {
 //
-//                  var searchField = $('#searchMovie').val();
-//                  var myExp = new RegExp(searchField, 'i');
+// $('#searchMovie').keydown(function(){
 //
-//              $.each(data, function(key, val) {
-//                var output = '<ul class="search-results">';
+//     $.getJSON(searchMovie,function(data){
 //
-//                  if (response.results.name.search(myExp) != -1) {
-//                    for(var i = 0, l = response.results.length / 1; i < l; i++) {
-//                      output += '<li>';
-//                      output += '<h2>' + response.results[i].name + '</h2>';
+//       var search = $('#searchMovie').val();
+//       var regex = new RegExp(search, 'i');
 //
-//                      output += '</li>';
-//                      }
-//                  }
-//              });
+//       $.each(data, function(key, val){
+//
+//         if((data.results.title.search(regex)) != -1 ){
+//
+//           for(var i = 0, l = results.length/1; i < l; i++) {
+//             var output;
+//             output += '<ul>';
+//              output += '<li>';
+//              output += '<h2>' + data.results[i].title + '</h2>';
+//
+//              output += '</li>';
 //              output += '</ul>';
-//              $('#results').html(output);
+//              $('#results').append(output);
 //
-//          });
-//
+//              }
+//         }
+//       });
+//     });
 //   });
+
+
+var searchMovie = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.themoviedb.org/3/search/multi?query=searchField&api_key=9e93a8072a4001746ef7cd2093ad1e4a",
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+}
+
+$.ajax(searchMovie).done(function (response) {
+console.log(this);
+               $('#searchMovie').keyup(function() {
+
+                 var searchField = $('#searchMovie').val();
+                 var myExp = new RegExp(searchField, 'i');
+
+             $.each(data, function(key, val) {
+               var output = '<ul class="search-results">';
+
+                 if (response.results.name.search(myExp) != -1) {
+                   for(var i = 0, l = response.results.length / 1; i < l; i++) {
+                     output += '<li>';
+                     output += '<h2>' + response.results[i].name + '</h2>';
+                     output += '</li>';
+                     }
+                 }
+             });
+             output += '</ul>';
+             $('#results').html(output);
+
+         });
+
+  });
 
 });
